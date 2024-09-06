63694671fe0432429cedc609990d94a5801a82c1
miguel bracamontes <bracamontesman@pm.me>
2024-09-06 17:14:32 -0600

    passed excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputReversed = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        let index = input[i]
+        inputReversed += index;
+    }
+    return inputReversed;
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
