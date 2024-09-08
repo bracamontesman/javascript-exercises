6e4e9ceea60c195488b78430441da2f40e58bf60
miguel bracamontes <bracamontesman@pm.me>
2024-09-07 18:02:39 -0600

    passed excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let eraser = array.filter(item => !toRemove.includes(item));
+    return eraser
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
